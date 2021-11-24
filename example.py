3cfb8341eeb5bcc9569edffa5b867e21ed5764f6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-11-24 12:33:06 +0600

    Add example

---
+++ example.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_strip_whitespace.middlewares import HTMLStripWhiteSpaceMiddleware
+
+
+app = Flask(__name__)
+
+app.wsgi_app = HTMLStripWhiteSpaceMiddleware(
+    app.wsgi_app,
+    # config={
+    #     "STRIP_WHITESPACE_RUST_MINIFY_CSS": False,
+    #     "STRIP_WHITESPACE_COMPRESSION_ALGORITHM": "plain",
+    # },
+)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+app.run(debug=True)
--- flask_strip_whitespace/middlewares/__init__.py
+++ flask_strip_whitespace/middlewares/__init__.py
@@ -19,12 +19,17 @@ except ImportError:
         """
     )
 
-from typing import Union
+from typing import Dict, Optional, Union
 from werkzeug.wrappers import Request, Response
+from flask import current_app
 
 
 class HTMLStripWhiteSpaceMiddleware(object):
-    def __init__(self, app):
+    def __init__(
+        self,
+        app,
+        config: Optional[Dict[str, bool | str]] = {},
+    ) -> None:
         self.app = app
 
         # Declare the variables that we need as None.
@@ -34,32 +39,75 @@ class HTMLStripWhiteSpaceMiddleware(object):
 
         # Set Some modifiable variables
 
-        self.STRIP_WHITESPACE_RUST_DO_NOT_MINIFY_DOCTYPE: bool
-        self.STRIP_WHITESPACE_RUST_ENSURE_SPEC_CONPLIANT_UNQUOTED_ATTRIBUTE_VALUES: bool
-        self.STRIP_WHITESPACE_RUST_KEEP_CLOSING_TAGS: bool
-        self.STRIP_WHITESPACE_RUST_KEEP_COMMENTS: bool
-        self.STRIP_WHITESPACE_RUST_KEEP_HTML_AND_HEAD_OPENING_TAGS: bool
-        self.STRIP_WHITESPACE_RUST_KEEP_SPACES_BETWEEN_ATTRIBUTES: bool
-        self.STRIP_WHITESPACE_RUST_MINIFY_CSS: bool
-        self.STRIP_WHITESPACE_RUST_MINIFY_JS: bool
-        self.STRIP_WHITESPACE_RUST_REMOVE_BANGS: bool = True
-        self.STRIP_WHITESPACE_RUST_REMOVE_PROCESSING_INSTRUCTIONS: bool = True
+        self.STRIP_WHITESPACE_RUST_DO_NOT_MINIFY_DOCTYPE: bool = config.get(
+            "STRIP_WHITESPACE_RUST_DO_NOT_MINIFY_DOCTYPE", True
+        )
+        self.STRIP_WHITESPACE_RUST_ENSURE_SPEC_CONPLIANT_UNQUOTED_ATTRIBUTE_VALUES: bool = config.get(
+            "STRIP_WHITESPACE_RUST_ENSURE_SPEC_CONPLIANT_UNQUOTED_ATTRIBUTE_VALUES",
+            True,
+        )
+        self.STRIP_WHITESPACE_RUST_KEEP_CLOSING_TAGS: bool = config.get(
+            "STRIP_WHITESPACE_RUST_KEEP_CLOSING_TAGS", True
+        )
+        self.STRIP_WHITESPACE_RUST_KEEP_COMMENTS: bool = config.get(
+            "STRIP_WHITESPACE_RUST_KEEP_COMMENTS", True
+        )
+        self.STRIP_WHITESPACE_RUST_KEEP_HTML_AND_HEAD_OPENING_TAGS: bool = config.get(
+            "STRIP_WHITESPACE_RUST_KEEP_HTML_AND_HEAD_OPENING_TAGS", True
+        )
+        self.STRIP_WHITESPACE_RUST_KEEP_SPACES_BETWEEN_ATTRIBUTES: bool = config.get(
+            "STRIP_WHITESPACE_RUST_KEEP_SPACES_BETWEEN_ATTRIBUTES", True
+        )
+        self.STRIP_WHITESPACE_RUST_MINIFY_CSS: bool = config.get(
+            "STRIP_WHITESPACE_RUST_MINIFY_CSS", True
+        )
+        self.STRIP_WHITESPACE_RUST_MINIFY_JS: bool = config.get(
+            "STRIP_WHITESPACE_RUST_MINIFY_JS", True
+        )
+        self.STRIP_WHITESPACE_RUST_REMOVE_BANGS: bool = config.get(
+            "STRIP_WHITESPACE_RUST_REMOVE_BANGS", True
+        )
+        self.STRIP_WHITESPACE_RUST_REMOVE_PROCESSING_INSTRUCTIONS: bool = config.get(
+            "STRIP_WHITESPACE_RUST_REMOVE_PROCESSING_INSTRUCTIONS", True
+        )
         # Python
-        self.STRIP_WHITESPACE_PYTHON_REMOVE_COMMENTS: bool
-        self.STRIP_WHITESPACE_PYTHON_CONDENSE_STYLE_FROM_HTML: bool
-        self.STRIP_WHITESPACE_PYTHON_CONDENSE_SCRIPT_FROM_HTML: bool
-        self.STRIP_WHITESPACE_PYTHON_CLEAN_UNNEEDED_HTML_TAGS: bool
-        self.STRIP_WHITESPACE_PYTHON_CONDENSE_HTML_WHITESPACE: bool
-        self.STRIP_WHITESPACE_PYTHON_UNQUOTE_HTML_ATTRIBUTES: bool
+        self.STRIP_WHITESPACE_PYTHON_REMOVE_COMMENTS: bool = config.get(
+            "STRIP_WHITESPACE_PYTHON_REMOVE_COMMENTS",
+            False,  # We do it in Rust. No need to do it in python
+        )
+        self.STRIP_WHITESPACE_PYTHON_CONDENSE_STYLE_FROM_HTML: bool = config.get(
+            "STRIP_WHITESPACE_PYTHON_CONDENSE_STYLE_FROM_HTML", True
+        )
+        self.STRIP_WHITESPACE_PYTHON_CONDENSE_SCRIPT_FROM_HTML: bool = config.get(
+            "STRIP_WHITESPACE_PYTHON_CONDENSE_SCRIPT_FROM_HTML", True
+        )
+        self.STRIP_WHITESPACE_PYTHON_CLEAN_UNNEEDED_HTML_TAGS: bool = config.get(
+            "STRIP_WHITESPACE_PYTHON_CLEAN_UNNEEDED_HTML_TAGS", True
+        )
+        self.STRIP_WHITESPACE_PYTHON_CONDENSE_HTML_WHITESPACE: bool = config.get(
+            "STRIP_WHITESPACE_PYTHON_CONDENSE_HTML_WHITESPACE", True
+        )
+        self.STRIP_WHITESPACE_PYTHON_UNQUOTE_HTML_ATTRIBUTES: bool = config.get(
+            "STRIP_WHITESPACE_PYTHON_UNQUOTE_HTML_ATTRIBUTES", True
+        )
         # NBSP char
-        self.STRIP_WHITESPACE_NBSP_MANGLE_CHARACTER: str
+        self.STRIP_WHITESPACE_NBSP_MANGLE_CHARACTER: str = config.get(
+            "STRIP_WHITESPACE_NBSP_MANGLE_CHARACTER", "অ"
+        )
         # Compression Settings
+
         self.STRIP_WHITESPACE_COMPRESSION_TYPE: Union[
             str("compressed"), str("decompressed")
-        ]
+        ] = config.get("STRIP_WHITESPACE_COMPRESSION_TYPE", str("decompressed"))
+
         self.STRIP_WHITESPACE_COMPRESSION_ALGORITHM: Union[
             str("gzip"), str("br"), str("zstd"), str("plain")
-        ]
+        ] = config.get(
+            "STRIP_WHITESPACE_COMPRESSION_ALGORITHM",
+            str(
+                "gzip",  # By default set it to GZ because its a python stdlib
+            ),
+        )
 
     def __compress__(
         self,
@@ -68,7 +116,10 @@ class HTMLStripWhiteSpaceMiddleware(object):
 
         return_buffer: bytes = b""
 
-        if self.STRIP_WHITESPACE_COMPRESSION_ALGORITHM == str("gzip"):
+        if self.STRIP_WHITESPACE_COMPRESSION_ALGORITHM == str("plain"):
+            return_buffer = buffer
+
+        elif self.STRIP_WHITESPACE_COMPRESSION_ALGORITHM == str("gzip"):
             try:
                 from python_strip_whitespace.functions.compressors.gzip import (
                     compress as gz_compress,
@@ -130,80 +181,8 @@ class HTMLStripWhiteSpaceMiddleware(object):
 
         return return_buffer
 
-    def __map_environ_to_variables__(self, environ: dict):
-        """
-        This module maps flask's environment variables to self
-            :params environ
-        """
-        self.STRIP_WHITESPACE_RUST_DO_NOT_MINIFY_DOCTYPE = environ.get(
-            "STRIP_WHITESPACE_RUST_DO_NOT_MINIFY_DOCTYPE", True
-        )
-        self.STRIP_WHITESPACE_RUST_ENSURE_SPEC_CONPLIANT_UNQUOTED_ATTRIBUTE_VALUES = (
-            environ.get(
-                "STRIP_WHITESPACE_RUST_ENSURE_SPEC_CONPLIANT_UNQUOTED_ATTRIBUTE_VALUES",
-                True,
-            )
-        )
-        self.STRIP_WHITESPACE_RUST_KEEP_CLOSING_TAGS = environ.get(
-            "STRIP_WHITESPACE_RUST_KEEP_CLOSING_TAGS", True
-        )
-        self.STRIP_WHITESPACE_RUST_KEEP_COMMENTS = environ.get(
-            "STRIP_WHITESPACE_RUST_KEEP_COMMENTS", True
-        )
-        self.STRIP_WHITESPACE_RUST_KEEP_HTML_AND_HEAD_OPENING_TAGS = environ.get(
-            "STRIP_WHITESPACE_RUST_KEEP_HTML_AND_HEAD_OPENING_TAGS", True
-        )
-        self.STRIP_WHITESPACE_RUST_KEEP_SPACES_BETWEEN_ATTRIBUTES = environ.get(
-            "STRIP_WHITESPACE_RUST_KEEP_SPACES_BETWEEN_ATTRIBUTES", True
-        )
-        self.STRIP_WHITESPACE_RUST_MINIFY_CSS = environ.get(
-            "STRIP_WHITESPACE_RUST_MINIFY_CSS", True
-        )
-        self.STRIP_WHITESPACE_RUST_MINIFY_JS = environ.get(
-            "STRIP_WHITESPACE_RUST_MINIFY_JS", True
-        )
-        self.STRIP_WHITESPACE_RUST_REMOVE_BANGS = environ.get(
-            "STRIP_WHITESPACE_RUST_REMOVE_BANGS", True
-        )
-        self.STRIP_WHITESPACE_RUST_REMOVE_PROCESSING_INSTRUCTIONS = environ.get(
-            "STRIP_WHITESPACE_RUST_REMOVE_PROCESSING_INSTRUCTIONS", True
-        )
-
-        self.STRIP_WHITESPACE_PYTHON_REMOVE_COMMENTS = environ.get(
-            "STRIP_WHITESPACE_PYTHON_REMOVE_COMMENTS",
-            False,  # We do it in Rust. No need to do it in python
-        )
-        self.STRIP_WHITESPACE_PYTHON_CONDENSE_STYLE_FROM_HTML = environ.get(
-            "STRIP_WHITESPACE_PYTHON_CONDENSE_STYLE_FROM_HTML", True
-        )
-        self.STRIP_WHITESPACE_PYTHON_CONDENSE_SCRIPT_FROM_HTML = environ.get(
-            "STRIP_WHITESPACE_PYTHON_CONDENSE_SCRIPT_FROM_HTML", True
-        )
-        self.STRIP_WHITESPACE_PYTHON_CLEAN_UNNEEDED_HTML_TAGS = environ.get(
-            "STRIP_WHITESPACE_PYTHON_CLEAN_UNNEEDED_HTML_TAGS", True
-        )
-        self.STRIP_WHITESPACE_PYTHON_CONDENSE_HTML_WHITESPACE = environ.get(
-            "STRIP_WHITESPACE_PYTHON_CONDENSE_HTML_WHITESPACE", True
-        )
-        self.STRIP_WHITESPACE_PYTHON_UNQUOTE_HTML_ATTRIBUTES = environ.get(
-            "STRIP_WHITESPACE_PYTHON_UNQUOTE_HTML_ATTRIBUTES", True
-        )
-        self.STRIP_WHITESPACE_NBSP_MANGLE_CHARACTER = environ.get(
-            "STRIP_WHITESPACE_NBSP_MANGLE_CHARACTER", "অ"
-        )
-        self.STRIP_WHITESPACE_COMPRESSION_TYPE = environ.get(
-            "STRIP_WHITESPACE_COMPRESSION_TYPE", str("decompressed")
-        )
-        self.STRIP_WHITESPACE_COMPRESSION_ALGORITHM = environ.get(
-            "STRIP_WHITESPACE_COMPRESSION_ALGORITHM",
-            str(
-                "gzip",  # By default set it to GZ because its a python stdlib
-            ),
-        )
-
-    def __call__(self, environ, start_response):
+    def __call__(self, environ: dict, start_response) -> Response:
         self.request: Request = Request(environ)
-        self.__map_environ_to_variables__(environ)
 
         def custom_start_response(status, headers, exc_info=None):
             accepted_encodings = self.request.headers.get(
